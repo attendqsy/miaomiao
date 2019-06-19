219695d4e56d8350d4cefcaf637653ac9f44f3c7
attendqsy <shaoyang_qin@126.com>
2019-06-19 15:07:15 +0800

    add setData

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import router from './routers'
 import store from './stores'
 
+import axios from 'axios';
+
+Vue.prototype.axios = axios;
+
+Vue.filter('setWH', (url, arg) => {
+    return url.replace(/w\.h/, arg);
+});
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
